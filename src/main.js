330f1dd40c0342d4dfa3c02de8ea62d3c37d1bfb
Krin <lightdark@bk.ru>
2020-12-01 00:29:09 +0300

    Добавлено закрытия поапа по нажатию esc

--- src/main.js
+++ src/main.js
@@ -37,7 +37,8 @@ render(footerStatisticsElement, new FooterStatistics().getElement(), `beforeend`
 
 
 const filmForDetails = films[0];
-render(mainElement, new FilmDetails(filmForDetails).getElement(), `beforeend`);
+const details = new FilmDetails(filmForDetails);
+render(mainElement, details.getElement(), `beforeend`);
 
 const commentsElement = document.querySelector(`.film-details__bottom-container`);
 const commentsForDetails = comments.filter((item) => (filmForDetails).comments.has(item.id));
@@ -45,9 +46,17 @@ if (commentsElement) {
   render(commentsElement, new Comments(commentsForDetails).getElement(), `beforeend`);
 }
 
-const details = document.querySelector(`.film-details`);
-const detailsCloseButton = details.querySelector(`.film-details__close-btn`);
-detailsCloseButton.addEventListener(`click`, () => details.remove());
+const onEscKeyDown = (evt) => {
+  if (evt.key === `Escape` || evt.key === `Esc`) {
+    evt.preventDefault();
+    details.getElement().remove();
+    document.removeEventListener(`keydown`, onEscKeyDown);
+  }
+};
+
+const detailsCloseButton = details.getElement().querySelector(`.film-details__close-btn`);
+detailsCloseButton.addEventListener(`click`, () => details.getElement().remove());
+document.addEventListener(`keydown`, onEscKeyDown);
 
 const filmListElement = mainElement.querySelector(`.films-list:not(.films-list--extra)`);
 const showMoreBtn = new ShowMore();
@@ -56,21 +65,19 @@ if (films.length > FILM_SHOWMORE_COUNT) {
   render(filmListElement, showMoreBtn.getElement(), `beforeend`);
 }
 
-if (showMoreBtn.getElement()) {
-  showMoreBtn.getElement().addEventListener(`click`, () => {
-    const filmContainerElement = mainElement.querySelector(`.films-list:not(.films-list--extra) .films-list__container`);
+showMoreBtn.getElement().addEventListener(`click`, () => {
+  const filmContainerElement = mainElement.querySelector(`.films-list:not(.films-list--extra) .films-list__container`);
 
-    films
+  films
       .slice(renderedFilmCount, renderedFilmCount + FILM_SHOWMORE_COUNT)
       .forEach((film) => render(filmContainerElement, new FilmCard(film).getElement(), `beforeend`));
 
-    renderedFilmCount += FILM_SHOWMORE_COUNT;
+  renderedFilmCount += FILM_SHOWMORE_COUNT;
 
-    if (renderedFilmCount >= films.length) {
-      showMoreBtn.getElement().remove();
-      showMoreBtn.removeElement();
-    }
-  });
-}
+  if (renderedFilmCount >= films.length) {
+    showMoreBtn.getElement().remove();
+    showMoreBtn.removeElement();
+  }
+});
 
 
